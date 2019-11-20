e9a0ee912ece031cee29af82a8be55b0ffd6dc2d
Ingo Augsten <info@ingo-augsten.de>
2019-11-20 22:36:23 +0100

    small fix (arg was renamed to players already)

--- warriorGame/GameLoop.cs
+++ warriorGame/GameLoop.cs
@@ -18,21 +18,24 @@ namespace warriorGame
 		private bool _gameOn = true;
 		private uint round = 1;
 
-		public void Start(ref List<Species> fighters)
+		public void Start(ref List<Species> players)
 		{
 			while (_gameOn)
 			{
 				Console.WriteLine(NewLine + "GAME ROUND " + round);
 
-				_gameOn &= Fight.FullFight(ref fighters);
-				_gameOn &= (fighters.Count > 1);
+				_gameOn &= Fight.FullFight(ref players);
+				_gameOn &= (players.Count > 1);
+
+				//fightOn &= Health.RecoverRound(players);
+
 
 				++round;
 				//if (round > 100)  break;
 			}
 
 			Console.WriteLine(NewLine + "GAME OVER, " +
-			                  Fight.GetSurvivedName(Fight.WhoSurvived(fighters)) +  // fighters = last pair TODO bissel heikel
+			                  Fight.GetSurvivedName(Fight.WhoSurvived(players)) +  // players = last pair TODO bissel heikel
 			                  " is left." + NewLine);
 		}
 	}
