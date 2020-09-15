c08bfd27e65c985999ddb7e0e3bfaa493853ac30
Ingo Augsten <info@ingo-augsten.de>
2020-09-15 13:04:00 +0200

    Comments

--- warriorGame/RndSrc.cs
+++ warriorGame/RndSrc.cs
@@ -5,7 +5,10 @@ namespace warriorGame
 {
 	public static class RndSrc
 	{
-		private static readonly Random _rnd = new Random();  // TODO Randomize/Constructor?
+		// RND helpers
+		// TODO improve Randomization etc, see Tim Coreys video  https://www.youtube.com/watch?v=kW84q8WOBdU
+		
+		private static readonly Random _rnd = new Random();
 
 
 		private static int GetInt(int max)  // 0..max-1
@@ -23,9 +26,10 @@ namespace warriorGame
 
 		public static double Vary(double baseVal, double plusMinusPercent, double maxVal)
 		{
-			// TODO not formula, base percentage differences on max, IAmTimCorey RND video
+			// scatter around baseVal -> baseVal +/- rnd(percent)
+			
 			double diffMax = baseVal * plusMinusPercent/Const.MaxPercent;
-			double rndVaried = baseVal - diffMax + (double) GetInt(Convert.ToInt32(2*diffMax));  // base +/- rnd(percent)
+			double rndVaried = baseVal - diffMax + (double) GetInt(Convert.ToInt32(2*diffMax));
 			if (rndVaried > maxVal)
 			{
 				rndVaried = maxVal;
@@ -49,13 +53,17 @@ namespace warriorGame
 
 		public static List<Species> PairFromList(ref List<Species> beings)
 		{
+			// Randomly choose and remove a pair of players from the list
+			
 			List<Species> pair = new List<Species>();  // TODO new necessary / difference?
 			int rnd;
 			
+			// first pick
 			rnd = RndSrc.GetInt(beings.Count);
 			pair.Add(beings[rnd]);
 			beings.RemoveAt(rnd);
 
+			// second pick
 			rnd = RndSrc.GetInt(beings.Count-1);
 			pair.Add(beings[rnd]);
 			beings.RemoveAt(rnd);
